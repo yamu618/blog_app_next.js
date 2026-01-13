f7d697475fe328b0d3a1602f686bc2cbe851c723
yamu <shoshuto0618@gmail.com>
2026-01-13 18:51:21 +0900

    プロジェクトの作成、初期設定

---
+++ my-app/prettier.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  singleQuote: true, // 文字列はシングルクオートを使用
+  semi: false, // 行末にセミコロンを付けない
+  tabWidth: 2, // インデントはスペース2つ
+  bracketSpacing: true, // オブジェクトの波括弧の間にスペースを入れる
+}
+
+export default config
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-800 mb-4">
+          Hello, Next.js!
+        </h1>
+        <p className="text-lg font-semibold text-gray-600 mt-2">
+          Next.js <span className="text-blue-500">15.4.6</span>
+        </p>
+      </div>
+    </main>
+  );
+}
